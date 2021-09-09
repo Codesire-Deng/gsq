aa987d5b55a6297801087fcf54e16798d320be4d
Zifeng Deng <oi_dzf@qq.com>
2021-09-09 22:18:02 +0800

    build: deprecate boost gil

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include <imgui/imgui.h>
 // #include "imgui/imgui_impl_glfw.h"
 // #include "imgui/imgui_impl_opengl3.h"
-#include <boost/gil.hpp>
+// #include <boost/gil.hpp>
 #include <Eigen/Dense>
 
 #include <iostream>
